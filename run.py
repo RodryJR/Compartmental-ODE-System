558117b14caa252e89b046c56bf2aefd5515ca57
RodryJR <dariordguezll@gmail.com>
2025-06-14 01:16:24 -0400

    save

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from genetic_experiment import*
 # --- Bloque principal para ejecutar el script ---
 if __name__ == "__main__":
     # Define aquí la configuración de un experimento específico
-    experiment_config = {
+    experiment_config_sir_model = {
         "input_filename": "src/data/SIR_noise_0p0.csv",
         "analysis_filename": "SIR_noise_0p0_exp1",
         "ga_params": {
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     
     # Llama a la función principal con la configuración
     run_genetic_experiment(
-        input_filename=experiment_config["input_filename"],
-        analysis_filename=experiment_config["analysis_filename"],
-        ga_params=experiment_config["ga_params"],
-        fitness_params=experiment_config["fitness_params"]
+        input_filename=experiment_config_sir_model["input_filename"],
+        analysis_filename=experiment_config_sir_model["analysis_filename"],
+        ga_params=experiment_config_sir_model["ga_params"],
+        fitness_params=experiment_config_sir_model["fitness_params"]
     )
